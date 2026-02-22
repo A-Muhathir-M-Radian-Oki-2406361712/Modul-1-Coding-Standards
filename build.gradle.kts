1acc403b3e5ebab8a128d5c057f4fe1b03450d92
Muhathir . <muhathirmro@gmail.com>
2026-02-23 03:45:50 +0700

    Fixing sonar system

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.5.10"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.sonarqube") version "6.0.1.5171"
+    id("org.sonarqube") version "7.0.1.6134"
 }
 
 group = "id.ac.ui.cs.advprog"
